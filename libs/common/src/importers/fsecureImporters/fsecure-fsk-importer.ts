892c6b595036b83ca07e89ca341b0c557f942354
Daniel James Smith <djsmith@web.de>
2022-11-03 13:02:16 +0100

    Extract method refactor
    Move logic inside of parse loop into parseEntry
    Extract handling of Entries of type Login into handleLoginEntry
    Extract handling of Entries of type CreditCard into handleCreditCardEntry

--- libs/common/src/importers/fsecureImporters/fsecure-fsk-importer.ts
+++ libs/common/src/importers/fsecureImporters/fsecure-fsk-importer.ts
@@ -1,10 +1,11 @@
 import { CipherType } from "../../enums/cipherType";
 import { ImportResult } from "../../models/domain/import-result";
 import { CardView } from "../../models/view/card.view";
+import { CipherView } from "../../models/view/cipher.view";
 import { BaseImporter } from "../baseImporter";
 import { Importer } from "../importer";
 
-import { FskEntryTypesEnum, FskFile } from "./types/fsecure-fsk-types";
+import { FskEntry, FskEntryTypesEnum, FskFile } from "./types/fsecure-fsk-types";
 
 export class FSecureFskImporter extends BaseImporter implements Importer {
   parse(data: string): Promise<ImportResult> {
@@ -22,44 +23,57 @@ export class FSecureFskImporter extends BaseImporter implements Importer {
       }
 
       const value = results.data[key];
-      const cipher = this.initLoginCipher();
-      cipher.name = this.getValueOrDefault(value.service);
-      cipher.notes = this.getValueOrDefault(value.notes);
-      cipher.favorite = value.favorite > 0;
-
-      switch (value.type) {
-        case FskEntryTypesEnum.Login:
-          cipher.login.username = this.getValueOrDefault(value.username);
-          cipher.login.password = this.getValueOrDefault(value.password);
-          cipher.login.uris = this.makeUriArray(value.url);
-          break;
-        case FskEntryTypesEnum.CreditCard:
-          cipher.type = CipherType.Card;
-          cipher.card = new CardView();
-          cipher.card.cardholderName = this.getValueOrDefault(value.username);
-          cipher.card.number = this.getValueOrDefault(value.creditNumber);
-          cipher.card.brand = this.getCardBrand(cipher.card.number);
-          cipher.card.code = this.getValueOrDefault(value.creditCvv);
-          if (!this.isNullOrWhitespace(value.creditExpiry)) {
-            if (!this.setCardExpiration(cipher, value.creditExpiry)) {
-              this.processKvp(cipher, "Expiration", value.creditExpiry);
-            }
-          }
-          if (!this.isNullOrWhitespace(value.password)) {
-            this.processKvp(cipher, "PIN", value.password);
-          }
-          break;
-        default:
-          continue;
-          break;
-      }
-
-      this.convertToNoteIfNeeded(cipher);
-      this.cleanupCipher(cipher);
+      const cipher = this.parseEntry(value);
       result.ciphers.push(cipher);
     }
 
     result.success = true;
     return Promise.resolve(result);
+  }
+
+  private parseEntry(entry: FskEntry): CipherView {
+    const cipher = this.initLoginCipher();
+    cipher.name = this.getValueOrDefault(entry.service);
+    cipher.notes = this.getValueOrDefault(entry.notes);
+    cipher.favorite = entry.favorite > 0;
+
+    switch (entry.type) {
+      case FskEntryTypesEnum.Login:
+        this.handleLoginEntry(entry, cipher);
+        break;
+      case FskEntryTypesEnum.CreditCard:
+        this.handleCreditCardEntry(entry, cipher);
+        break;
+      default:
+        return;
+        break;
+    }
+
+    this.convertToNoteIfNeeded(cipher);
+    this.cleanupCipher(cipher);
+    return cipher;
+  }
+
+  private handleLoginEntry(entry: FskEntry, cipher: CipherView) {
+    cipher.login.username = this.getValueOrDefault(entry.username);
+    cipher.login.password = this.getValueOrDefault(entry.password);
+    cipher.login.uris = this.makeUriArray(entry.url);
+  }
+
+  private handleCreditCardEntry(entry: FskEntry, cipher: CipherView) {
+    cipher.type = CipherType.Card;
+    cipher.card = new CardView();
+    cipher.card.cardholderName = this.getValueOrDefault(entry.username);
+    cipher.card.number = this.getValueOrDefault(entry.creditNumber);
+    cipher.card.brand = this.getCardBrand(cipher.card.number);
+    cipher.card.code = this.getValueOrDefault(entry.creditCvv);
+    if (!this.isNullOrWhitespace(entry.creditExpiry)) {
+      if (!this.setCardExpiration(cipher, entry.creditExpiry)) {
+        this.processKvp(cipher, "Expiration", entry.creditExpiry);
+      }
+    }
+    if (!this.isNullOrWhitespace(entry.password)) {
+      this.processKvp(cipher, "PIN", entry.password);
+    }
   }
 }
